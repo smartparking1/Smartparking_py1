b6f90ae22ec09804295cc3d1560fc44dff9f87b9
Saiteja-Dunaboyina <129719672+Saiteja-Dunaboyina@users.noreply.github.com>
2023-06-27 12:37:32 +0530

    vehicle

--- Smartparking/Smartparking/urls.py
+++ Smartparking/Smartparking/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('Employee/',include('employee_authentication.urls')),
     path('building/',include('BuildingDetails.urls')),
-    path('vehical/',include('Vehicalparkingapp.urls'))
+    path('vehicleparking/',include('Vehicalparkingapp.urls'))
 
 ]
