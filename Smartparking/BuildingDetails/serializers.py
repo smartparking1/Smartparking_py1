835effed19a89a9a0c01512fc182d941305195af
Saiteja-Dunaboyina <129719672+Saiteja-Dunaboyina@users.noreply.github.com>
2023-07-01 15:58:29 +0530

    floor

--- Smartparking/BuildingDetails/serializers.py
+++ Smartparking/BuildingDetails/serializers.py
@@ -8,32 +8,32 @@ class BuildingSerializer(serializers.ModelSerializer) :
         fields = '__all__'
 
 class FloorSerializer(serializers.ModelSerializer) :
-    building = BuildingSerializer()
+    # building = BuildingSerializer()
     class Meta :
         model = FloorDetails
         fields = '__all__'
     
-    def create(self, validated_data):
-        building_data = validated_data.pop('building')
-        building = BuildingDetails.objects.filter(**building_data).first()
-        floor = FloorDetails.objects.create(building=building,**validated_data)
-        return floor
+    # def create(self, validated_data):
+    #     building_data = validated_data.pop('building')
+    #     building = BuildingDetails.objects.filter(**building_data).first()
+    #     floor = FloorDetails.objects.create(building=building,**validated_data)
+    #     return floor
     
 class FloorsDetailsSerializer(serializers.ModelSerializer):
-    building = BuildingSerializer(read_only=True)
+    # building = BuildingSerializer(read_only=True)
 
     class Meta:
         model = FloorDetails
         fields = '__all__'
 
     
-    def create(self, validated_data):
-        building_data = validated_data.pop('building')
-        building_id = building_data.get('building_id')
-        building, _ = BuildingDetails.objects.get_or_create(building_id=building_id, defaults=building_data)
-        validated_data['building'] = building
-        floor = FloorDetails.objects.create(**validated_data)
-        return floor
+    # def create(self, validated_data):
+    #     building_data = validated_data.pop('building')
+    #     building_id = building_data.get('building_id')
+    #     building, _ = BuildingDetails.objects.get_or_create(building_id=building_id, defaults=building_data)
+    #     validated_data['building'] = building
+    #     floor = FloorDetails.objects.create(**validated_data)
+    #     return floor
 
 class SlotSerializer(serializers.ModelSerializer):
     floor = FloorSerializer()
