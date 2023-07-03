16d025213f76d6876c45bbe40d5fec840218b722
Saiteja-Dunaboyina <129719672+Saiteja-Dunaboyina@users.noreply.github.com>
2023-07-03 17:44:51 +0530

    vehicle

--- Smartparking/Vehicalparkingapp/urls.py
+++ Smartparking/Vehicalparkingapp/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('insertvehicleparking/',InsertVehicleParking.as_view(),name='insertvehicleparking'),
-    path('updatevehicleparking/<int:empId>/',UpdateVehicleParking.as_view(),name='updatevehicleparking'),
+    path('updatevehicleparking/<int:empId>/<str:vehicle_no>/',UpdateVehicleParking.as_view(),name='updatevehicleparking'),
     path('updatefineamount/',UpdateFineAmount.as_view(),name='updatefineamount'),
     path('getallvehicleparking/',GetAllVehicles.as_view(),name='getallvehicleparking'),
 ]
